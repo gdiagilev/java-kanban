631bf3e237e6bb19a2b91ed7b1e3f41767763b72
gdiagilev <gdiagilev@yandex.ru>
2025-02-18 01:15:34 +0300

    Правки в методе getAllSubtasks();

--- src/ru/yandex/tracker/Model/Node.java
+++ src/ru/yandex/tracker/Model/Node.java
@@ -6,6 +6,7 @@ public class Node<T extends Task> {
     public T task;
     public Node<T> next;
     public Node<T> prev;
+
     public Node(Node<T> prev, T task, Node<T> next) {
         this.task = task;
         this.next = next;
