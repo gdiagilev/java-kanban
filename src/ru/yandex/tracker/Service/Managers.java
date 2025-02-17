3c0ca303284afdd3f585ce6091f72ce8e0afe423
gdiagilev <gdiagilev@yandex.ru>
2025-02-18 02:17:15 +0300

    Правки в методе getAllSubtasks();

--- src/ru/yandex/tracker/Service/Managers.java
+++ src/ru/yandex/tracker/Service/Managers.java
@@ -4,7 +4,7 @@ public class Managers {
     public TaskManager getDefault() {
         return new InMemoryTaskManager();
     }
-    public static HistoryManager getDefaultHistory(){
+    public static HistoryManager getDefaultHistory() {
         return new InMemoryHistoryManager();
     }
 }
