9727bab5f9199c75b84aad8974c4bb6c938faa8b
gdiagilev <gdiagilev@yandex.ru>
2025-11-08 18:25:56 +0300

    Добавлена новая функциональность: трекер задач расставляет задачи по приоритету и проверяет их на пересечение по времени выполнения. ДЛя новых функциональностей добавлены тесты.

--- src/ru/yandex/tracker/Service/InMemoryHistoryManager.java
+++ src/ru/yandex/tracker/Service/InMemoryHistoryManager.java
@@ -17,7 +17,7 @@ public class InMemoryHistoryManager implements HistoryManager {
         if (historyMap.containsKey(task.getId())) {
             removeNode(historyMap.get(task.getId()));
         }
-        
+
         linkLast(task);
     }
 
