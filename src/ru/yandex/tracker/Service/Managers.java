0e246ef219b19cecbab87e5923d2b265554d46b0
gdiagilev <gdiagilev@yandex.ru>
2025-11-08 19:29:54 +0300

    Добавлена новая функциональность: трекер задач расставляет задачи по приоритету и проверяет их на пересечение по времени выполнения. ДЛя новых функциональностей добавлены тесты.

--- src/ru/yandex/tracker/Service/Managers.java
+++ src/ru/yandex/tracker/Service/Managers.java
@@ -4,13 +4,11 @@ import java.io.File;
 
 public class Managers {
 
-    private static final HistoryManager DEFAULT_HISTORY_MANAGER = new InMemoryHistoryManager();
-
     public TaskManager getDefault() {
         return new FileBackedTaskManager(new File("./resources/kanban.csv"));
     }
 
     public static HistoryManager getDefaultHistory() {
-        return DEFAULT_HISTORY_MANAGER; // один и тот же объект
+        return new InMemoryHistoryManager();
     }
 }
