1453d4ad72a7ab10d08cc937e284fc881fd16d5a
gdiagilev <gdiagilev@yandex.ru>
2025-11-22 08:10:02 +0300

    Настроены доступы к методам программы через HTTP API.

--- src/ru/yandex/tracker/Service/Managers.java
+++ src/ru/yandex/tracker/Service/Managers.java
@@ -6,14 +6,18 @@ import java.nio.file.Paths;
 public class Managers {
 
     public static FileBackedTasksManager getFileBackedManager(Path file) {
-        return new FileBackedTasksManager(file) {
-        };
+        return new FileBackedTasksManager(file);
     }
 
     public static FileBackedTasksManager getDefault() {
-        Path defaultPath = Paths.get("tasks.csv"); // стандартный файл
+        Path defaultPath = Paths.get("tasks.csv");
         return getFileBackedManager(defaultPath);
     }
+
+    public static InMemoryHistoryManager getDefaultHistory() {
+        return new InMemoryHistoryManager();
+    }
+
 
     public static InMemoryTaskManager getInMemoryManager() {
         return new InMemoryTaskManager();
