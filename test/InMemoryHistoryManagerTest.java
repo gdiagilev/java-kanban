ba9fa7f1c6039c791603c2cd42d2f043de9864b1
gdiagilev <gdiagilev@yandex.ru>
2025-11-22 08:31:47 +0300

    Настроены доступы к методам программы через HTTP API.

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -18,7 +18,7 @@ class InMemoryHistoryManagerTest {
 
     @BeforeEach
     void setUp() {
-        historyManager = Managers.getDefault().getHistoryManager();
+        historyManager = Managers.getDefaultHistory();
 
         task1 = new Task("Task 1", "Description 1", Status.NEW,
                 LocalDateTime.now(), Duration.ofMinutes(15));
