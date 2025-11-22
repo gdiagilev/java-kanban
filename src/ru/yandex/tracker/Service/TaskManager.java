2c33038135c71a1e8a808e315f8ee24c0abc26ed
gdiagilev <gdiagilev@yandex.ru>
2025-11-22 04:49:09 +0300

    Настроены доступы к методам программы через HTTP API.

--- src/ru/yandex/tracker/Service/TaskManager.java
+++ src/ru/yandex/tracker/Service/TaskManager.java
@@ -7,34 +7,49 @@ import java.util.List;
 public interface TaskManager {
 
     Task getTask(int id) throws NotFoundException;
+
     Epic getEpicTask(int id) throws NotFoundException;
+
     Subtask getSubtask(int id) throws NotFoundException;
 
     void createTask(Task task);
+
     void createEpicTask(Epic epicTask);
+
     void createSubtask(Subtask subtask);
 
     List<Subtask> getAllSubTasksByEpicId(int epicId) throws NotFoundException;
 
     void deleteTask(Task task) throws NotFoundException;
+
     void deleteEpicTask(Epic epicTask) throws NotFoundException;
+
     void deleteSubtask(Subtask subTask) throws NotFoundException;
 
     void deleteTask(int id) throws NotFoundException;
+
     void deleteEpicTask(int id) throws NotFoundException;
+
     void deleteSubtask(int id) throws NotFoundException;
 
     void deleteAllTasks();
+
     void deleteAllEpicTasks();
+
     void deleteAllSubtasks();
 
     void updateTask(Task task) throws NotFoundException;
+
     void updateEpicTask(Epic epicTask) throws NotFoundException;
+
     void updateSubtask(Subtask subTask) throws NotFoundException;
 
     List<Task> getAllTasks();
+
     List<Epic> getAllEpicTasks();
+
     List<Subtask> getAllSubtasks();
+
     List<Task> getHistory();
 
     List<Task> getPrioritizedTasks();
