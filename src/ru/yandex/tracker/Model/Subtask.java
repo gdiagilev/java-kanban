4b624a4eec4dc7f4f04105fdbfa875e3a0fb69e9
gdiagilev <gdiagilev@yandex.ru>
2025-11-22 06:50:37 +0300

    Настроены доступы к методам программы через HTTP API.

--- src/ru/yandex/tracker/Model/Subtask.java
+++ src/ru/yandex/tracker/Model/Subtask.java
@@ -7,16 +7,26 @@ public class Subtask extends Task {
 
     private final int epicId;
 
-    public Subtask(int epicId, String name, String description, Status status,
-                   LocalDateTime startTime, Duration duration) {
+    public Subtask(
+            int epicId,
+            String name,
+            String description,
+            Status status,
+            LocalDateTime startTime,
+            Duration duration
+    ) {
         super(name, description, status, startTime, duration);
         this.epicId = epicId;
     }
 
-    public int getEpicId() { return epicId; }
+    public int getEpicId()
+    {
+        return epicId;
+    }
 
     @Override
-    public TaskType getTaskType() {
+    public TaskType getTaskType()
+    {
         return TaskType.SUBTASK;
     }
 }
